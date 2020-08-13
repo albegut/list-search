d4034c540ab4c0f6e8beb6b3b17b6ea89fc552b6
Gutiérrez Pérez, Alberto <agutierrezp@minsait.com>
2020-08-13 11:14:42 +0000

    Initial commit

---
+++ list-search-webpart/src/webparts/listSearch/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IListSearchWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ListSearchWebPartStrings' {
+  const strings: IListSearchWebPartStrings;
+  export = strings;
+}
