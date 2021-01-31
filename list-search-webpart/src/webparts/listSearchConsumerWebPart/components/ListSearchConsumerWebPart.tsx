e9045ca3b2ed016f51f8567aa7f18df0519c97e1
Gutiérrez Pérez, Alberto <agutierrezp@minsait.com>
2021-01-31 12:16:17 +0000

    fix new webpart styles

--- list-search-webpart/src/webparts/listSearchConsumerWebPart/components/ListSearchConsumerWebPart.tsx
+++ list-search-webpart/src/webparts/listSearchConsumerWebPart/components/ListSearchConsumerWebPart.tsx
@@ -6,25 +6,23 @@ export default class ListSearchConsumer extends React.Component<IListSearchConsu
   public render(): React.ReactElement<IListSearchConsumerProps> {
     return (
       <div className={styles.listSearchConsumerWebPart}>
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.column}>
-              <span className={styles.title}>List search consumer webpart</span>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <span className={styles.title}>List search consumer webpart</span>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.description}>WebUrl:
+                <p className={styles.value}>{this.props.webUrl}</p>
             </div>
-            <div className={styles.column}>
-              <div className={styles.description}>WebUrl:
-                <p>{this.props.webUrl}</p>
-              </div>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.description}>ListId:
+                <p className={styles.value}>{this.props.listId}</p>
             </div>
-            <div className={styles.column}>
-              <div className={styles.description}>ListId:
-                <p>${this.props.listId}</p>
-              </div>
-            </div>
-            <div className={styles.column}>
-              <div className={styles.description}>ItemId:
-                <p>${this.props.itemId}</p>
-              </div>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.description}>ItemId:
+                <p className={styles.value}>{this.props.itemId}</p>
             </div>
           </div>
         </div>
